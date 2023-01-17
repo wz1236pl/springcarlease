313de49c003ee43a208ec34c3aa3f1854b1f6d0f
Lukasz <dziq243@gamil.com>
2023-01-17 16:56:59 +0100

    clear project

---
+++ src/main/java/projekt/carlease/springcarlease/SpringcarleaseApplication.java
@@ -0,0 +1,13 @@
+package projekt.carlease.springcarlease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcarleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcarleaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projekt/carlease/springcarlease/SpringcarleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package projekt.carlease.springcarlease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcarleaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
